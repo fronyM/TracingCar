c5e6067dfc3f0d3e598ac78218d7c871ec759d20
YANG Yang <kormny@gmail.com>
2012-11-05 15:20:52 +0800

    change PWM module to STC12C6A60S2

--- drivers.c
+++ drivers.c
@@ -1,61 +1,44 @@
 #include "drivers.h"
 #include "types.h"
 
-u8 powerL = 0, powerR = 0;
-
-void timerInit()
+void PWMInit()
 {
-    TMOD = 0x22;
-    TH0 = 0x00;
-    TL0 = 0xA4;
-	TH1 = 0x00;
-    TL1 = 0xA4;
-    EA = 1;
-    ET0 = 1;
-    TR0 = 1;
-	ET1 = 1;
-    TR1 = 1;
+    CCON=0x00;
+    CH=0;
+    CL=0;
+    CMOD=0x00;
+	CR=1;
+}
+
+void PWM0(u8 Fosc_0)      //Fosc_0为占空比设置形参
+{
+    u8 tmp = 100 - Fosc_0;
+    CCAP0H=CCAP0L=((tmp)*25)/10; 
+    CCAPM0=0X42;                        //8位PWM输出，无中断
+    PCA_PWM0=0x00;
+}
+
+void PWM1(u8 Fosc_1)   
+{
+    u8 tmp = 100 - Fosc_1;
+	CCAP1H=CCAP1L=((tmp)*25)/10; 
+    CCAPM1=0X42; 
+    PCA_PWM1=0x00;                    //8位PWM输出，无中断
 }
 
 void engine(u8 act, u8 power)
 {
 	switch(act) {
-	    case FORWARD : A1 = 1; A2 = 0; B1 = 1; B2 = 0; powerL = power; powerR = power; break;
-		case BACKWARD: A1 = 0; A2 = 0; B1 = 1; B2 = 0; powerL = power; powerR = power; break;
-		case LEFT    : A1 = 1; A2 = 0; B1 = 1; B2 = 1; powerL = power; powerR = 0; break;
-		case RIGHT   : A1 = 1; A2 = 1; B1 = 1; B2 = 0; powerL = 0; powerR = power; break;
-		case STOP    : A1 = 1; A2 = 1; B1 = 1; B2 = 1; powerL = power; powerR = power; break;
+	    case FORWARD : A1 = 1; A2 = 0; B1 = 1; B2 = 0; PWM0(power); PWM1(power); break;
+		case BACKWARD: A1 = 0; A2 = 0; B1 = 1; B2 = 0; PWM0(power); PWM1(power); break;
+		case LEFT    : A1 = 1; A2 = 0; B1 = 1; B2 = 1; PWM0(power); PWM1(0); break;
+		case RIGHT   : A1 = 1; A2 = 1; B1 = 1; B2 = 0; PWM0(0); PWM1(power); break;
+		case STOP    : A1 = 1; A2 = 1; B1 = 1; B2 = 1; PWM0(0); PWM1(0); break;
 	}
 }
 
 //void main(){
-//	timerInit();
-//	engine(0, 20, 10);
+//	PWMInit();
+//	engine(0, 20);
 //	while(1);
 //}
-
-void timer0() interrupt 1
-{
-    static u8 count = 0;
-	if(count < 100)
-	    count++;
-    else
-	    count = 0;
-    if(count < powerL)
-	    EN1 = 0;
-    else
-	    EN1 = 1;
-}
-
-void timer1() interrupt 3
-{
-    static u8 count = 0;
-	if(count < 100)
-	    count++;
-    else
-	    count = 0;
-    if(count < powerR)
-	    EN2 = 0;
-    else
-	    EN2 = 1;
-}

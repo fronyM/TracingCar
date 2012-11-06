ee74ace30c3344c7f8dd9c36e847de233b06348a
Frosch <86375088@qq.com>
2012-11-07 00:33:36 +0800

    oh shit

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
-#include "drivers.h"
-#include "tracing/tracing.h"
-#include "zigbee/zigbee.h"
-#include "zigbee/car.h"
+#include "../head/drivers.h"
+#include "../head/tracing.h"
+#include "../head/zigbee.h"
+#include "../head/car.h"
+#include "../head/1602.h"
 #include <reg51.h>
 
 void init()
 {
 	init_serial();
+	init_1602();
 	PWMInit();
 	found_path(T_STAT);
-	while(!(sensor_0 || sensor_1 || sensor_2 || sensor_3 || sensor_4 || sensor_5));
+	while(!(T_RIGHT || T_LEFT || T_MID));
 }
 
 void main()
 {
 	init();
+//	write_data(0x31);
+//	write_com(0x80+0x40);
+//	write_char("1602&595");
+
+
 	tracing(1);
 	engine(STOP, 99);
+
 	while(1);
 	{
 	//	engine(FORWARD, 99);

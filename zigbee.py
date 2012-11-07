b044d93df998ef9572786e4920de0098c79dfd7c
YANG Yang <kormny@gmail.com>
2012-11-07 14:25:20 +0800

    bugs fixed

--- zigbee.py
+++ zigbee.py
@@ -12,41 +12,44 @@ sendinfo = [0xff, 0xff, 0x01, 0x01, 0x00]
 def getResponse():
     response = zigbee.read(5)
     response = [ord(c) for c in response]
-    if(response[0] == 0xff and response[1] = 0xff):
-        print 'Car ' + response[2] + ' returned error'
-        sys.exit(1)
+    print response
+    if(not(response[0] == 0xff and response[1] == 0x01)):
+        sys.stderr.write('Car ' + str(response[2]) + ' returned error\n')
+        sys.exit(2)
     if(response[4] == 0x00):
-        print 'Car ' + response[2] + ' got a tool from ' + places[response[3]]
+        print 'Car ' + str(response[2]) + ' got a tool from ' + places[response[3]]
     elif(response[4] == 0x01):
-        print 'Car ' + response[2] + ' parking at ' + places[response[3]]
+        print 'Car ' + str(response[2]) + ' parking at ' + places[response[3]]
     elif(response[4] == 0x02):
-        print 'Car ' + response[2] + ' returned at Gateway. '
+        print 'Car ' + str(response[2]) + ' returned at Gateway. '
     else:
         sys.stderr.write('received data incorrcet.\n')
         sys.exit(2)
 
 def send(car, dest1, dest2):
-    sendinfo[2] = car
-    sendinfo[3] = dest1
-    sendinfo[4] = dest2
-    sendinfo = array.array('B', sendinfo).tostring
-    zigbee.write(sendinfo)
-    if(zigbee.read() != 0xaa):
-        sys.stderr.write('communication erron.\n')
+    sendbuf = sendinfo
+    sendbuf[2] = int(car)
+    sendbuf[3] = dest1
+    sendbuf[4] = dest2
+    sendbuf = array.array('B', sendbuf).tostring()
+    zigbee.write(sendbuf)
+    ack = zigbee.read()
+    if(ord(ack) != dest1):
+        sys.stderr.write('communication error.\n')
         sys.exit(2)
 
 def select():
     carNo = raw_input("Chose one car you want to control: \n")
-    storage1 = raw_input("Chose the first place you want to:\n")
-    storage2 = raw_input("Chose the second place you want to:\n")
-    storage1 += 2
-    storage2 += 2
+    s1 = raw_input("Chose the first place you want to:\n")
+    s2 = raw_input("Chose the second place you want to:\n")
+    storage1 = int(s1) + 2
+    storage2 = int(s2) + 2
     send(carNo, storage1, storage2) 
 
 def main():
-    zigbee = serial.Serial('/dev/ttyUSB0')
     select()
-    response()
+    getResponse()
 
 if __name__ == '__main__':
+    zigbee = serial.Serial('/dev/ttyUSB0')
     main()
